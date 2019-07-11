2f6770bf9a5bf09e68ea3ef322ddad09039648d4
Dengnaiwen <634379240@qq.com>
2019-07-11 11:19:01 +0800

    蜗牛商城第一次提交

---
+++ WoniuShop/src/main/java/com/home/woniushop/servlet/Demo.java
@@ -0,0 +1,22 @@
+package com.home.woniushop.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/demo")
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("user");
+		response.getWriter().print(username);
+		response.getWriter().flush();
+		response.getWriter().close();
+	}
+
+}
